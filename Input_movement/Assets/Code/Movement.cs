dbe80fc4c5634c19b140b358007a5bc63f872c35
Erik Forssell <Erikeforssell@gmail.com>
2023-09-17 02:18:58 +0200

    Revert "Update to movement"
    This reverts commit 617e462da7f80bf5b7a99271f3722b7aca35c3c3.

--- Input_movement/Assets/Code/Movement.cs
+++ Input_movement/Assets/Code/Movement.cs
@@ -7,9 +7,9 @@ using UnityEngine.UI;
 public class Movement : MonoBehaviour
 {
     //Public variables that wer can edit in the editor
-    private float maxSpeed = 5; //Our max speed
-    private float acceleration = 5; //How fast we accelerate
-    private float deacceleration = 4; //brake power
+    public float maxSpeed = 5; //Our max speed
+    public float acceleration = 20; //How fast we accelerate
+    public float deacceleration = 4; //brake power
     int points = 0;
     public GameObject Coin;
     [SerializeField] private TextMeshProUGUI textMeshProUGUI;
@@ -22,11 +22,11 @@ public class Movement : MonoBehaviour
     Vector2 position; //our position
 
 
-    
+    Rigidbody2D rb20;
 
     private void Start()
     {
-        
+        rb20 = GetComponent<Rigidbody2D>();
 
         
     }
@@ -35,25 +35,23 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+        velocity.Normalize();
         //Get the raw input
         rawInput.x = Input.GetAxisRaw("Horizontal");
         rawInput.y = Input.GetAxisRaw("Vertical");
 
-        //If we have a square magnitude over one, normalize the length of the vector to 1
-        //if it's shorter then one we don't need this step.
         if (rawInput.sqrMagnitude > 1)
         {
             rawInput.Normalize();
         }
 
-        //add our input to our velocity
-        //This provides accelleration +10m/s/s
+
         velocity += rawInput * acceleration * Time.deltaTime;
 
-        //Check our max speed, if our magnitude is faster them max speed
+ 
         if (velocity.sqrMagnitude > maxSpeed * maxSpeed)
         {
-            //Normalize our velocity and change the magnitude to our max speed.
+
             velocity.Normalize();
             velocity *= maxSpeed;
         }
@@ -61,17 +59,14 @@ public class Movement : MonoBehaviour
         //If we have zero input from the player
         if (rawInput.sqrMagnitude == 0)
         {
-            //Reduce our speed based on how fast we are going
-            //A value of 0.9 would remove 10% or our speed
+
             velocity *= 1 - deacceleration * Time.deltaTime;
         }
 
-        //Update our position vector with our movement over time
         position += velocity * Time.deltaTime;
-
-        //Move our transform based to our updated position.
-        transform.position = position;
         
+
+        rb20.velocity = velocity;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
